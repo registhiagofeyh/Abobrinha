dbfb009d21e6fc675ee41ce777231f993b84133e
Andreluiz04 <andre.luiz_1985@hotmail.com>
2014-05-10 18:45:17 -0300

    Update soma.java

--- soma.java
+++ soma.java
@@ -1,8 +1,13 @@
-class Soma{
-	private int c;
-
-	public soma(int a, int b){
-		c = a+b;
-		System.out.println("soma: "+c);
-	}
+public void calculaSoma(String linhas[], int a){
+		int auxiliar=0, auxiliar1=0, r1, r, i, t, s, k;
+		double cont=0, cont1=0;
+		String aux,aux3,aux4; 		
+		
+		t=linhas[a].indexOf("=");
+		aux=linhas[a].substring(0,t);
+		s=linhas[a].indexOf(";");
+		k=linhas[a].indexOf("+");
+														
+		aux3=linhas[a].substring(t+1,k);
+		aux4=linhas[a].substring(k+1,s);					
 }
